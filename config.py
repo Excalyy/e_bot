78a78396c000148a032ad57f953b9a273a640de1
Dmitry Davidenko <nazarbaevdimon@gmail.com>
2025-11-29 14:16:02 +0500

    Отредактировал конфиг под работу с .env

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-TOKEN = '7912011135:AAH0cAZbpwnEG3NyV66rF0TEaNKJgxu30sg'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('BOT_TOKEN')
+BASE_URL = os.getenv('BASE_URL', 'https://oksei.ru/studentu/raspisanie_uchebnykh_zanyatij')
