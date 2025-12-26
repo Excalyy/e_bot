cdb45f89bddad8eb71caf72d07a05beadfe4f24c
Dmitry Davidenko <nazarbaevdimon@gmail.com>
2025-12-27 00:42:40 +0500

    удалил лишние файлы

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv('BOT_TOKEN')
-BASE_URL = os.getenv('BASE_URL', 'https://oksei.ru/studentu/raspisanie_uchebnykh_zanyatij')
